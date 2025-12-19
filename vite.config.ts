d6174f87d0429924f18e6422b05abd10ad83f1c9
KiranMantha <kiranv.mantha@gmail.com>
2025-12-19 09:11:57 +0530

    changed assets folder to docs root

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import preact from "@preact/preset-vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: "/astro-software/",
   build: {
     target: "esnext", // Modern browsers; adjust if needed
     minify: "esbuild", // Efficient minification
     sourcemap: false, // Disable for production (optional)
-    assetsDir: "",
+    // assetsDir: "",
     outDir: "docs",
   },
 });
