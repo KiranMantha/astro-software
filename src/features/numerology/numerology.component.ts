31141fd913319dd76d3cd8ea132462f974275f9f
KiranMantha <kiranv.mantha@gmail.com>
2026-06-12 10:12:51 +0530

    code refactoring

--- src/features/numerology/numerology.component.ts
+++ src/features/numerology/numerology.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NUMEROLOGY_LETTER_SCORES } from '../../data/constants';
 
 @Component({
   selector: 'app-numerology',
@@ -13,36 +14,6 @@ export class NumerologyPyramidComponent {
   inputName: string = '';
   lettersRow: string[] = [];
   pyramidRows: number[][] = [];
-
-  // Chaldean Numerology Mapping
-  private letterValues: { [key: string]: number } = {
-    A: 1,
-    I: 1,
-    J: 1,
-    Q: 1,
-    Y: 1,
-    B: 2,
-    K: 2,
-    R: 2,
-    C: 3,
-    G: 3,
-    L: 3,
-    S: 3,
-    D: 4,
-    M: 4,
-    T: 4,
-    E: 5,
-    H: 5,
-    N: 5,
-    X: 5,
-    U: 6,
-    V: 6,
-    W: 6,
-    O: 7,
-    Z: 7,
-    F: 8,
-    P: 8
-  };
 
   generatePyramid(): void {
     // 1. Clean input: keep only alphabets and convert to uppercase
@@ -58,7 +29,7 @@ export class NumerologyPyramidComponent {
     this.lettersRow = cleanedName.split('');
 
     // 3. Generate the first numeric row based on the letter mapping
-    const firstNumericRow = this.lettersRow.map((char) => this.letterValues[char] || 0);
+    const firstNumericRow = this.lettersRow.map((char) => NUMEROLOGY_LETTER_SCORES[char] || 0);
 
     this.pyramidRows = [firstNumericRow];
 
